36e1d5267bf0a0601d87e7a3d2a689fbc386ed65
Kuker <Kuker@KUKER>
2016-12-28 12:34:33 +0100

    Added client side History view

--- KennelUnion/KennelUnion.Web/Controllers/InfoController.cs
+++ KennelUnion/KennelUnion.Web/Controllers/InfoController.cs
@@ -12,10 +12,12 @@ namespace KennelUnion.Web.Controllers
     public class InfoController : Controller
     {
         private readonly IRepository<About> _aboutRepo;
+        private readonly IRepository<History> _historyRepo;
 
-        public InfoController(IRepository<About> aboutRepo)
+        public InfoController(IRepository<About> aboutRepo, IRepository<History> historyRepo)
         {
             _aboutRepo = aboutRepo;
+            _historyRepo = historyRepo;
         }
 
         public IActionResult About()
@@ -26,7 +28,8 @@ namespace KennelUnion.Web.Controllers
 
         public IActionResult History()
         {
-            return View();
+            var history = _historyRepo.GetAll();
+            return View(history.Last());
         }
 
         public IActionResult Contact()
