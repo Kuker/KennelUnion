8be9511fc120a1b69bc450badeeed2f98964c860
Kuker <Kuker@KUKER>
2016-12-28 10:52:35 +0100

    Implemented browsing and accepting dog registries

--- KennelUnion/KennelUnion.Web/Controllers/AdminPanelController.cs
+++ KennelUnion/KennelUnion.Web/Controllers/AdminPanelController.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using KennelUnion.Data.Entities;
 using KennelUnion.Data.Repositories;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc;
@@ -12,15 +13,18 @@ using Microsoft.EntityFrameworkCore;
 
 namespace KennelUnion.Web.Controllers
 {
+    [Authorize]
     public class AdminPanelController : Controller
     {
         private readonly IRepository<News> _newsRepository;
+        private readonly IRepository<DogRegistry> _dogRegistryRepository;
         private readonly UserManager<IdentityUser> _userManager;
 
-        public AdminPanelController(UserManager<IdentityUser> userManager, IRepository<News> newsRepository)
+        public AdminPanelController(UserManager<IdentityUser> userManager, IRepository<News> newsRepository, IRepository<DogRegistry> dogRegistryRepository)
         {
             this._userManager = userManager;
             _newsRepository = newsRepository;
+            _dogRegistryRepository = dogRegistryRepository;
         }
 
         public IActionResult Index()
@@ -93,6 +97,29 @@ namespace KennelUnion.Web.Controllers
         {
             var news = _newsRepository.GetById(id);
             return View(news);
+        }
+
+        public IActionResult BrowseDogRegistries(int page = 1, int maxPerPage = 15)
+        {
+            ViewBag.PagesCount = (int) Math.Ceiling((decimal) _dogRegistryRepository.GetAll().Count()/maxPerPage);
+            ViewBag.Page = page;
+            ViewBag.MaxPerPage = maxPerPage;
+            var registries = _dogRegistryRepository.GetAll()
+                .Include(x => x.Breeder)
+                .Include(x => x.Dog)
+                .OrderByDescending(x => x.CreatedOn)
+                .Skip((page - 1)*maxPerPage)
+                .Take(maxPerPage);
+            return View(registries);
+        }
+
+        public IActionResult AcceptRegistry(int id = 0)
+        {
+            var registry = _dogRegistryRepository.GetById(id);
+            registry.IsApproved = true;
+            _dogRegistryRepository.Edit(registry);
+            _dogRegistryRepository.Save();
+            return RedirectToAction("BrowseDogRegistries");
         }
     }
 }
